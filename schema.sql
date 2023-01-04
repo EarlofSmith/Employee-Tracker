bf7156871456e2efcea409ea037e0b54e0d58e1e
EarlofSmith <r1smith3333@gmail.com>
2023-01-04 16:54:39 -0700

    added schema and the tables for each

---
+++ schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  department_name VARCHAR (30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+  id INT,
+  title VARCHAR (30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL,
+    first_name VARCHAR (30),
+    last_name VARCHAR (30),
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id),
+    REFERENCES roles(id)
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+
+)
