a6713133ad8973fbc053402c434b067751494445
EarlofSmith <r1smith3333@gmail.com>
2023-01-12 11:13:32 -0700

    got adds to work on most of the replace and adds

--- index.js
+++ index.js
@@ -101,19 +101,23 @@ const addRole = () => {
                 }
               
         ]).then ((info) => {
-            const newRole= 'INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)'
-            const dpartmentID = 
-            // need some kind of join here to turn department name into department id
-            db.query(newRole, [info.add_role, info.add_salary, info.addRoleDepartment], (err)=>{
+            const dpartmentID = 'SELECT id FROM departments WHERE department_name = ?'
+            db.query(dpartmentID, info.addRoleDepartment, (err,res) =>{
                 if (err) {
-                    throw err;
+                    throw err
                 }
-                rolesArray.push(info.addRole)
-                console.log('You have successfully added a new role')
-                console.table(info);
-                overview();
-        })
-
+                const idDepartment = res[0].id
+                const newRole= 'INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)'
+                 db.query(newRole, [info.add_role, info.add_salary, idDepartment], (err)=>{
+                    if (err) {
+                        throw err;
+                    }
+                    rolesArray.push(info.addRole)
+                    console.log('You have successfully added a new role')
+                    console.table(info);
+                    overview();
+                })
+            })
             })
             
        
@@ -141,7 +145,17 @@ const addRole = () => {
                 message: 'What is the manager ID number of the employee you would you like to add?',
                 name: 'manager'
             }
-        ]).then()
+        ]).then((info) => {
+            const newEmployee = `INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`
+            db.query(newEmployee,[info.first_name, info.last_name, info.role, info.manager], (err) => {
+                if (err) {
+                    throw err;
+                }
+                console.log('You have successfully added a new employee')
+                console.table(info)
+                overview();
+            })
+        }) 
     };
     
     const updateRole = () => {
@@ -159,7 +173,25 @@ const addRole = () => {
                 name: 'new_role',
                 choices: rolesArray,
             }
-        ])
+        ]).then((info)=>{
+            const idUpdated = `SELECT id FROM roles WHERE title = ?`
+            db.query(idUpdated, info.new_role,(err,res) =>{
+                if (err) {
+                    throw err;
+                }
+                const roleID = res[0].id;
+                const roleUpdate = `UPDATE employees SET role_id = ? WHERE first_name = ?`
+                db.query(roleUpdate, [roleID, info.update_role], (err, res) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log('')
+                    console.log('You have successfully updated a employees role')
+                    console.log([roleID, info.update_role])
+                    overview();
+                })
+            })
+        })
     };
     
     const addDepartment = () => {
@@ -169,7 +201,20 @@ const addRole = () => {
                     message: 'What is the name of the Department you would like to add?',
                     name: 'add_department'
                 }
-            ])
+            ]).then((info) => {
+                const newDepartment = `INSERT INTO departments (new_department) VALUES (?)`
+                db.query(newDepartment, info.add_department, (err) => {
+                    if (err) {
+                        throw err
+                    }
+                    console.log('')
+                    console.log('You have successfully created a new department')
+                    console.log(`The department ${info} has been added`)
+                    console.log('')
+                    departmentsArray.push(info.add_department)
+                    overview();
+                })
+            })
     };
 
     const employeesArray = [];
